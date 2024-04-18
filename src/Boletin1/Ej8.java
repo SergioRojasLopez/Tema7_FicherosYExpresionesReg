fa386cef22da2a59cf7dfdcfd89c882faa25713c
Sergio Rojas <sergiorojaslopez12@gmail.com>
2024-04-18 13:45:18 +0200

    Ejercicio 8 modificado

--- src/Boletin1/Ej8.java
+++ src/Boletin1/Ej8.java
@@ -5,11 +5,8 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 public class Ej8 {
     public static void main(String[] args) {
