250ba60dc666b787b054db99e3a385a8097222ed
Sergio Rojas <sergiorojaslopez12@gmail.com>
2024-04-18 14:40:59 +0200

    Ejercicio 8 modificado Y Ejercicio 9

--- src/Boletin1/Ej8.java
+++ src/Boletin1/Ej8.java
@@ -7,14 +7,15 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class Ej8 {
     public static void main(String[] args) {
         Path path = Paths.get(".\\src\\Boletin1\\pruebaEj8");
         Pattern pt = Pattern.compile("((?:\\p{L}{2,}\\s){3})(([1-9][0-9]?)|0[1-9])");
 
-        try (BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
-            if (reader.lines().map(pt::matcher).allMatch(Matcher::matches)) {
+        try (Stream<String> stream = Files.lines(path)) {
+            if (stream.map(pt::matcher).allMatch(Matcher::matches)) {
                 System.out.println("El fichero es valido");
             } else {
                 System.out.println("El fichero no es valido");
---
+++ src/Boletin1/Ej9.java
@@ -0,0 +1,40 @@
+package Boletin1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+public class Ej9 {
+    public static void main(String[] args) {
+
+        Path path = Paths.get(".\\src\\Boletin1\\matriculas");
+        Path path2 = Paths.get(".\\src\\Boletin1\\matriculasNuevas");
+        Pattern pt = Pattern.compile("\\p{L}+\\s(\\d{4}-[A-Z&&[^AEIOU]]{3})");
+
+        try (BufferedWriter writer = Files.newBufferedWriter(path2, StandardOpenOption.TRUNCATE_EXISTING,StandardOpenOption.CREATE);
+             Stream<String> lineas = Files.lines(path)) {
+            lineas.map(pt::matcher)
+                    .filter(Matcher::matches)
+                    .forEach(matcher -> {
+                        try {
+                            writer.write(matcher.group(1));
+                            writer.newLine();
+                        } catch (IOException e) {
+                            throw new RuntimeException("Ha ocurrido un error");
+                        }
+                    });
+
+        } catch (IOException e) {
+            throw new RuntimeException("Ha ocurrido un error");
+        }
+    }
+}
